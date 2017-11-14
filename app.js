7f01415f9c4e6f61e01b5d28c264ad3136a6525a
xiangdingyi <33444487+xiangdingyi@users.noreply.github.com>
2017-11-14 09:50:31 +0800

    init

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 //app.js
 App({
-  
+  globalData: {
+
+  },
+  onLaunch() {
+    wx.request({
+      url: 'https://resources.ninghao.net/wxapp-case/db.json',
+      success: (response) => {
+        Object.assign(this.globalData, response.data)
+
+        const currentPages = getCurrentPages()
+        if (currentPages.length !== 0) {
+          currentPages[currentPages.length - 1].onLoad()
+        }
+      }
+    })
+  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,24 @@
 //index.js
 //获取应用实例
+import testDrive from '../../modules/test-drive'
 const app = getApp()
 
 Page({
-  
+  data: {
+    slides: null,
+    entities: null
+  },
+  testDrive,
+  readMore(event) {
+    wx.navigateTo({
+      url: `/pages/vehicles/show?id=${event.target.dataset.id}`
+    })
+  },
+  onLoad() {
+    this.setData({
+      slides: app.globalData.slides,//幻灯片的获取
+      entities: app.globalData.vehicles
+    })
+  }
 })
+
